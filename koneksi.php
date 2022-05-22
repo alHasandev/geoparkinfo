368069b9c62744bb6e34d81d17d29ecbd8993851
alHasandev <alhasan99dev@gmail.com>
2022-05-22 14:52:31 +0800

    test: handle mysql connection error

--- koneksi.php
+++ koneksi.php
@@ -6,3 +6,8 @@ $PASSWORD = getenv('PASSWORD') ?: 'root';
 $DBNAME = getenv('DBNAME') ?: 'geopark';
 
 $koneksi = new mysqli($HOST, $USERNAME, $PASSWORD, $DBNAME);
+
+// Handle connection error
+if ($koneksi->connect_error) {
+  die("Connection failed: " . $koneksi->connect_error);
+}
