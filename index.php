a53b3b0fc4aa6151536faf0ec2dbb384a40648af
alHasandev <alhasan99dev@gmail.com>
2022-05-25 11:06:40 +0800

    test: set content-security-policy value

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $file = @$_GET['file'] ?: 'index';
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <meta name="description" content="" />
   <meta name="author" content="" />
-  <meta http-equiv="Content-Security-Policy" content="default-src * self blob: data: gap:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap:; connect-src self * 'unsafe-inline' blob: data: gap:; frame-src * self blob: data: gap:;">
+  <meta http-equiv="Content-Security-Policy" content="default-src * 'self' blob: https://geoparkinfo.alhasandev.com data: gap:; style-src * 'self' 'unsafe-inline' blob: https://geoparkinfo.alhasandev.com data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: https://geoparkinfo.alhasandev.com data: gap:; object-src * 'self' blob: https://geoparkinfo.alhasandev.com data: gap:; img-src * 'self' 'unsafe-inline' blob: https://geoparkinfo.alhasandev.com data: gap:; connect-src 'self' * 'unsafe-inline' blob: https://geoparkinfo.alhasandev.com data: gap:; frame-src * 'self' blob: https://geoparkinfo.alhasandev.com data: gap:;">
   <title>Geopark Info - ULM Informasi Geografis</title>
   <!-- Favicon-->
   <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
